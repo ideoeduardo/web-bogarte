478292397802963fd4c26f521e64609b18ada109
ideoeduardo <ideonapi@gmail.com>
2024-06-27 08:58:45 -0400

    agregado-de-carpeta-quiltro02-y-mas
    agregado-de-carpeta-quiltro02-y-mas

---
+++ quiltro02/ake.php
@@ -0,0 +1,119 @@
+
+
+
+
+<?php 
+//mi_pagina aka.php
+header('Content-Type: text/html; charset=utf-8');
+header ("Cache-Control: no-cache, must-revalidate ");
+?>
+<!--nota01-->
+		<div class="span-8 divicolor_01">
+            <div class="d33">
+              <img src="quiltro02/img/pez.jpg" alt="pez" width="96" height="77" class="rollover img-right" title="."  />
+        </div>
+            <div class="d66">
+              <p class="divitex">Delegados y Delegadas de Prevención se reúnen con funcionarios del Inpsasel </p>
+            </div>
+		<div class="clear"></div>
+	    </div>
+<!--nota01-->
+		<div class="span-8 divicolor_01 last">
+            <div class="d33">
+              <img src="quiltro02/img/pez.jpg" alt="pez" width="96" height="77" class="rollover img-right" title="."  />
+        </div>
+            <div class="d66">
+              <p class="divitex">Delegados y Delegadas de Prevención se reúnen con funcionarios del Inpsasel </p>
+            </div>
+		<div class="clear"></div>
+	    </div>
+<hr class="space02"></hr>
+<!--nota01-->
+		<div class="span-8 divicolor_01">
+            <div class="d33">
+              <img src="quiltro02/img/pez.jpg" alt="pez" width="96" height="77" class="rollover img-right" title="."  />
+        </div>
+            <div class="d66">
+              <p class="divitex">Delegados y Delegadas de Prevención se reúnen con funcionarios del Inpsasel </p>
+            </div>
+		<div class="clear"></div>
+	    </div>
+<!--nota01-->
+		<div class="span-8 divicolor_01 last">
+            <div class="d33">
+              <img src="quiltro02/img/pez.jpg" alt="pez" width="96" height="77" class="rollover img-right" title="."  />
+        </div>
+            <div class="d66">
+              <p class="divitex">Delegados y Delegadas de Prevención se reúnen con funcionarios del Inpsasel </p>
+            </div>
+		<div class="clear"></div>
+	    </div>
+<hr class="space02"></hr>
+<!--nota01-->
+		<div class="span-8 divicolor_01">
+            <div class="d33">
+              <img src="quiltro02/img/pez.jpg" alt="pez" width="96" height="77" class="rollover img-right" title="."  />
+        </div>
+            <div class="d66">
+              <p class="divitex">Delegados y Delegadas de Prevención se reúnen con funcionarios del Inpsasel </p>
+            </div>
+		<div class="clear"></div>
+	    </div>
+<!--nota01-->
+		<div class="span-8 divicolor_01 last">
+            <div class="d33">
+              <img src="quiltro02/img/pez.jpg" alt="pez" width="96" height="77" class="rollover img-right" title="."  />
+        </div>
+            <div class="d66">
+              <p class="divitex">Delegados y Delegadas de Prevención se reúnen con funcionarios del Inpsasel </p>
+            </div>
+		<div class="clear"></div>
+	    </div>
+<hr class="space02"></hr>
+<!--nota01-->
+		<div class="span-8 divicolor_01">
+            <div class="d33">
+              <img src="quiltro02/img/pez.jpg" alt="pez" width="96" height="77" class="rollover img-right" title="."  />
+        </div>
+            <div class="d66">
+              <p class="divitex">Delegados y Delegadas de Prevención se reúnen con funcionarios del Inpsasel </p>
+            </div>
+		<div class="clear"></div>
+	    </div>
+<!--nota01-->
+		<div class="span-8 divicolor_01 last">
+            <div class="d33">
+              <img src="quiltro02/img/pez.jpg" alt="pez" width="96" height="77" class="rollover img-right" title="."  />
+        </div>
+            <div class="d66">
+              <p class="divitex">Delegados y Delegadas de Prevención se reúnen con funcionarios del Inpsasel </p>
+            </div>
+		<div class="clear"></div>
+	    </div>
+<hr class="space02"></hr>
+<!--nota01-->
+		<div class="span-8 divicolor_01">
+            <div class="d33">
+              <img src="quiltro02/img/pez.jpg" alt="pez" width="96" height="77" class="rollover img-right" title="."  />
+        </div>
+            <div class="d66">
+              <p class="divitex">Delegados y Delegadas de Prevención se reúnen con funcionarios del Inpsasel </p>
+            </div>
+		<div class="clear"></div>
+	    </div>
+<!--nota01-->
+		<div class="span-8 divicolor_01 last">
+            <div class="d33">
+              <img src="quiltro02/img/pez.jpg" alt="pez" width="96" height="77" class="rollover img-right" title="."  />
+        </div>
+            <div class="d66">
+              <p class="divitex">Delegados y Delegadas de Prevención se reúnen con funcionarios del Inpsasel </p>
+            </div>
+		<div class="clear"></div>
+	    </div>
+    
+    
+
+    
+    
+    
